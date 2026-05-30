c2640b4e8e0622445649029c4a2d1a7bd4d53f49
J Srushti <srushtiachar61@gmail.com>
2026-05-30 15:09:38 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        
+        List<Integer> list=new ArrayList<>();
+        for(int a:stones){
+            list.add(a);
+        }
+
+        while(list.size()>1)
+        {
+            Collections.sort(list);
+            int a=list.remove(list.size()-1);
+            int b=list.remove(list.size()-1);
+
+            list.add(Math.abs(a-b));
+        }
+        return list.isEmpty() ? 0 : list.get(0);
+    }
+}
