ad8b8219c6637b9a71016e96cbf41b06c9657918
J Srushti <srushtiachar61@gmail.com>
2026-06-21 14:24:38 +0530

    Add: rotate-array - submission-1

---
+++ Data Structures & Algorithms/rotate-array/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void swap(int[] nums, int l, int r){
+        while(l<=r){
+            int temp=nums[l];
+            nums[l]=nums[r];
+            nums[r]=temp;
+            l++;
+            r--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+        
+        k = k % nums.length;
+        swap(nums,0,nums.length-1);
+        swap(nums,0,k-1);
+        swap(nums,k,nums.length-1);
+    }
+}
